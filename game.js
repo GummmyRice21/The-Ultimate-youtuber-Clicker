872726f0c9d77bbd7b80dade22de9d26d941f4dc
GummmyRice21 <swilliams4@brooklynsteamcenter.org>
2024-05-28 14:53:30 -0400

    NEW update to buttons and colors

--- game.js
+++ game.js
@@ -4,7 +4,13 @@ let camera = document.getElementById("camera")
 let cameraOwned = document.getElementById("cameraOwned")
 let computer = 0
 let computerOwned = document.getElementById("computerOwned")
+let storage  =0
 let strorge = document.getElementById("storage")
+let Headphones = 0
+let headphones = document.getElementById("headphones")
+let headphonesOwned = document.getElementById("headphonesOwned")
+
+
 
 
 function increaseMoney() {
@@ -12,6 +18,7 @@ function increaseMoney() {
 
     tracker.innerText = points.toFixed(2) + " Dollars"
 }
+
 
 function buyCamera () {
      if(points >=4000) {
@@ -25,25 +32,63 @@ function buyCamera () {
      }
 }
 
-
-
-
 function buycomputer () {
     if(points >=4000) {
        points = points - 4000
        tracker.innerText = points + ' Dollars'
        computer = computer + 1 
-       computerOwned.innerText = 'computers Owned: ' + computer
+       computerOwned.innerText = 'Computers Owned: ' + computer
        } else {
            alert("You Broke My GUY GET A JOB")
 
     }
 }
 
+function buystorage () {
+    if ( points >=4000){
+        points = points - 4000
+        tracker.innerText = points + 'Dollars'
+        storage = strorge +  1
+        storageOwned.innerText = 'storage Owned;  ' + storage
+        } else {
+         alert("You Broke My GUY GET A JOB")    
+    }
+}
+
+function buyheadphones () {
+    if ( points >=4000){
+        points = points - 4000
+        tracker.innerText = points + 'Dollars'
+        headphones = headphones +  1
+        headphonesOwned.innerText = 'headphones Owned;  ' + headphones
+        } else {
+         alert("You Broke My GUY GET A JOB")    
+    }
+}
 
 function gameloop() {
     points = points + (200 * camera)
+    points = points + (200 * computer)
+    points = points + (200 * storage)
+    points = points + (200 * headphones)
     tracker.innerText = points.toFixed(2) + '$ Dollars'
 }
+// setInterval (gameloop, 1000)
 
+// function gameloop() {
+//     points = points + (200 * Computer)
+//     tracker.innerText = points.toFixed(2) + '$ Dollars'
+// }
+// setInterval (gameloop, 1000)
+
+// function gameloop() {
+//     points = points + (200 * storage)
+//     tracker.innerText = points.toFixed(2) + '$ Dollars'
+// }
+// setInterval (gameloop, 1000)
+
+// function gameloop() {
+//     points = points + (200 * Headphones)
+//     tracker.innerText = points.toFixed(2) + '$ Dollars'
+// }
 setInterval (gameloop, 1000)
