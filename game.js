814d32f3855e38405aef69ee93250cd2a7194eeb
GummmyRice21 <swilliams4@brooklynsteamcenter.org>
2024-06-03 14:10:25 -0400

    Dynamic Pricing,Enabling/Disabling of buttons.

--- game.js
+++ game.js
@@ -3,16 +3,20 @@ let tracker = document.getElementById("tracker")
 let camera = document.getElementById("camera")
 let cameraCost = document.getElementById("cameraCost")
 let cameraOwned = document.getElementById("cameraOwned")
+let cameraButton = document.getElementById("cameraButton")
 let computer = 0
 let computerOwned = document.getElementById("computerOwned")
 let computerCost = document.getElementById("computerCost")
+let computerButton = document.getElementById("computerButton")
 let storage  =0
 let strorge = document.getElementById("storage")
 let storageCost = document.getElementById("storageCost")
+let storageButton = document.getElementById("storageButton")
 let Headphones = 0
 let headphones = document.getElementById("headphones")
 let headphonesOwned = document.getElementById("headphonesOwned")
 let headphonesCost = document.getElementById("headphonesCost")
+let headphonesButton = document.getElementById("headphonesButton")
 
 
 
@@ -20,21 +24,21 @@ let headphonesCost = document.getElementById("headphonesCost")
 
 
 function increaseMoney() {
-    points = points + 200
+    points = points + 7
 
     tracker.innerText = points.toFixed(2) + "$ Dollars"
 }
 
 
 function buyCamera () {
-    let cost = 400 + camera ** 5  
-     if(points >=cost) {
-        points = points - cost
+    let costCamera = 400 + camera ** 5  
+     if(points >= costCamera) {
+        points = points - costCamera
         tracker.innerText = points.toFixed(2) + '$ Dollars'
         camera = camera + 1 
         cameraOwned.innerText = 'Cameras Owned: ' + camera
-        cost = 400 + camera ** 5
-        cameraCost.innerText = cost.toFixed(0) + "$ Dollars"
+        costCamera = 400 + camera ** 5
+        cameraCost.innerText = costCamera.toFixed(0) + "$ Dollars"
         } else {
             alert("You Broke My GUY GET A JOB")
 
@@ -42,15 +46,15 @@ function buyCamera () {
 }
 
 function buycomputer () {
-    let cost = 1500 + computer ** 5  
-    if(points >=cost) {
-       points = points - cost
+    let costComputer = 1500 + computer ** 5  
+    if(points >= costComputer) {
+       points = points - costComputer
        tracker.innerText = points.toFixed(2) + '$ Dollars'
        tracker.innerText = points + ' Dollars'
        computer = computer + 1 
        computerOwned.innerText = 'Computers Owned: ' + computer
-       cost = 1500 + computer ** 5
-       computerCost.innerText = cost.toFixed(0) + "$ Dollars"
+       costComputer = 1500 + computer ** 5
+       computerCost.innerText = costComputer.toFixed(0) + "$ Dollars"
        } else {
            alert("You Broke My GUY GET A JOB")
 
@@ -58,22 +62,26 @@ function buycomputer () {
 }
 
 function buystorage () {
-    if ( points >=4000){
-        points = points - 4000
-        tracker.innerText = points + 'Dollars'
+    let costStorage = 4000 + storage ** 5
+    if ( points >= costStorage){
+        points = points - costStorage
+        tracker.innerText = points.toFixed(2) + 'Dollars'
         storage = strorge +  1
         storageOwned.innerText = 'storage Owned;  ' + storage
+        storageCost.innerText = costStorage.toFixed(0) + "$ Dollars"
         } else {
          alert("You Broke My GUY GET A JOB")    
     }
 }
 
 function buyheadphones () {
-    if ( points >=120){
-        points = points - 120
-        tracker.innerText = points + 'Dollars'
+    let costheadphones = 120 + headphones ** 5
+    if ( points >= costheadphones){
+        points = points - costheadphones
+        tracker.innerText = points.toFixed(2) + 'Dollars'
         headphones = headphones +  1
         headphonesOwned.innerText = 'headphones Owned;  ' + headphones
+        headphonesCost.innerText = costheadphones.toFixed(2) + "$ Dollars"
         } else {
          alert("You Broke My GUY GET A JOB")    
     }
@@ -86,5 +94,36 @@ function gameloop() {
     points = points + (2 * headphones)
     tracker.innerText = points.toFixed(2) + '$ Dollars'
 }
+
+
+function checkPrice() {
+    if (points.toFixed(0) >= 400 + camera ** 5) {
+        cameraButton.disabled = false
+    }
+    else {
+        cameraButton.disabled = true
+    }
+
+    if (points.toFixed(0) >= 1500 + computer ** 5) {
+        computerButton.disabled = false
+    }
+    else {
+        computerButton.disabled = true
+    }
+    if (points.toFixed(0) >= 4000 + storage ** 5) {
+        storageButton.disabled = false
+    }
+    else {
+        storageButton.disabled = true
+    }
+    if (points.toFixed(0) >= 150 + headphones ** 5) {
+        headphonesButton.disabled = false
+    }
+    else {
+        headphonesButton.disabled = true
+    }
+}
+
 setInterval (gameloop, 1000)
 
+setInterval (checkPrice, 10)
